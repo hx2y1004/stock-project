6831edb9d803446b7268f1f95d08ffc1c17af170
hx2y1004 <73586238+hx2y1004@users.noreply.github.com>
2024-12-21 20:23:12 +0900

    Fix: Add frontend as normal directory

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 상단 기초 템플릿 혹시 몰라 남겨둔다 . 
+
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>주식 모의 투자 플랫폼</h1>
+            <p>실시간 데이터를 기반으로 모의 투자를 즐겨보세요!</p>
+        </div>
+    );
+}
+
+export default App;
+
+
